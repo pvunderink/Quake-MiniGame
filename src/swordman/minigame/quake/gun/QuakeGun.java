20ca8bc6315780df7de31251d9c4fd5fe92a6896
DevPeppi <pj.vunderink@gmail.com>
2015-11-08 11:06:24 +0100

    Bukkitified

--- src/swordman/minigame/quake/gun/QuakeGun.java
+++ src/swordman/minigame/quake/gun/QuakeGun.java
@@ -3,8 +3,7 @@ package swordman.minigame.quake.gun;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.md_5.bungee.api.ChatColor;
-
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
