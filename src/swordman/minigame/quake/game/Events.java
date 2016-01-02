a283f97eb07d60d43641a216a0099393a2a48905
Kasteelheertje <unconfigured@null.spigotmc.org>
2016-01-02 14:50:40 +0100

    edited some stuff

--- src/swordman/minigame/quake/game/Events.java
+++ src/swordman/minigame/quake/game/Events.java
@@ -1,14 +1,22 @@
 package swordman.minigame.quake.game;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerInventoryEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import swordman.minigame.api.arena.Arena;
+import swordman.minigame.api.event.ArenaStartEvent;
+import swordman.minigame.api.event.ArenaStopEvent;
 import swordman.minigame.quake.Main;
 import swordman.minigame.quake.gun.GunHandler;
 import swordman.minigame.quake.gun.QuakeGun;
@@ -41,5 +49,27 @@ public class Events implements Listener {
 			p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 10, gun.getWalkSpeed()), true);
 		}
 	}
+	@EventHandler
+	public void onGameStart(final ArenaStartEvent ev) {
+		Arena arena = ev.getArena();
+		for (Player p : arena.getPlayers()) {
+			p.getInventory().addItem(QuakeGun.WOOD.getItemStack());
+			p.getInventory().setHeldItemSlot(0);
+		}
+	}
+	@EventHandler
+	public void onGameFinish(final ArenaStopEvent ev) {
+		Arena arena = ev.getArena();
+		for (Player p : arena.getPlayers()) {
+			p.getInventory().remove(QuakeGun.WOOD.getItemStack());
+		}
+		}
+	
+	@EventHandler
+	public void onItemSwitch(final InventoryMoveItemEvent ev) {
+		if (ev.getItem() == QuakeGun.WOOD.getItemStack()) {
+			ev.setCancelled(true);
+		}
+	}
+	}
 
-}
